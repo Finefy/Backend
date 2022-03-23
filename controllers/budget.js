0fc15744e60c0be8e145d0409bce28019edbfbaf
mahekvora21 <dr.vorajasmina@gmail.com>
2022-03-23 23:19:29 +0530

    update files

--- controllers/budget.js
+++ controllers/budget.js
@@ -2,6 +2,9 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const Budget = require("../models/budget");
+var MongoClient = require('mongodb').MongoClient;
+var url = "";
+
 
 const list = async (req, res, next) => {
     const email = req.email;
@@ -24,28 +27,22 @@ const list = async (req, res, next) => {
       next(err);
     }
   };
-/*
+
   const add = async (req, res, next) => {
     const email = req.email;
-  
-    try {
-      const budget = await Budget.findOne({ email: email })
-  
-      if (!email) {
-        const err = new Error("User not found!");
-        err.statusCode = 401;
-        throw err;
-      }
-  
-      res.status(200).json({
-        message: "Added!",
-        email: budget.email,
-        budget:budget.budget
-      });
-    } catch (err) {
-      next(err);
-    }
-  };
+    const budget = req.budget;
+  MongoClient.connect(url, function(err, db) {
+    
+    if (err) throw err;
+    var dbo = db.db("mydb");
+    var myobj = { email: email, budget: budget };
+    dbo.collection("customers").insertOne(myobj, function(err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+    });
+  });
+};
+ 
   // router.post("/budget/add", isAuth, budgetController.add);
 // router.put("/budget/update", isAuth, budgetController.update);
-*/
