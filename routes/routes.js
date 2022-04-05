a51bc75b5f2cce6962e8e2fdd5c5804fb2be8801
micepram <76100223+micepram@users.noreply.github.com>
2022-04-05 22:15:00 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -10,6 +10,16 @@ const transactionsController = require("../controllers/transaction");
 const usersController = require("../controllers/user");
 const router = express.Router();
 
+// set up rate limiter: maximum of five requests per minute
+var RateLimit = require('express-rate-limit');
+var limiter = new RateLimit({
+  windowMs: 1*60*1000, // 1 minute
+  max: 5
+});
+
+// apply rate limiter to all requests
+express.use(limiter);
+
 const emailValidator = body("email")
   .isEmail()
   .normalizeEmail()
