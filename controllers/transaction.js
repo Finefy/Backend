8b779f830a1ebc3b332a0cdf716fe5a3625486dc
Pramika Garg <micepram@gmail.com>
2022-04-05 21:53:45 +0530

    Updated Transactions

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -1,61 +1,97 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const Transaction = require("../models/transaction");
+const SingleTrans = require("../models/singleTrans");
 
+var MongoClient = require('mongodb').MongoClient;
+var url = "";
 
-const addTransaction = async (req, res, next) => {
-    try {
-      const transType = req.body.transType;
-      const amount = req.body.amount;
-      const category =req.body.category;
-  
-      // const errors = validationResult(req);
-      // if (!errors.isEmpty()) {
-      //   const errArray = errors.array();
-      //   const err = new Error(errArray[0].msg);
-      //   err.statusCode = 422;
-      //   err.data = errArray;
-      //   throw err;
-      // }
-  
+const list = async (req, res, next) => {
+  const userId = req.userId;
+  try {
+    const user = await User.findById(userId);
+    const transactions = await User.findById(userId).populate("transactions");
+    if (!userId || !user) {
+      const err = new Error("User Unauthenticated!");
+      err.statusCode = 401;
+      throw err;
+    }
+    res.status(200).json({
+      message: "Transactions Fetched!",
+      transactions: transactions.trans
+    });
+  } catch (err) {
+    next(err);
+  }
+};
 
+const add = async (req, res, next) => {
+  const userId = req.userId;
+  const transactions_type = req.body.transactions_type;
+  const transactions_amount = req.body.transactions_amount;
+  const transactions_category = req.body.transactions_category;
+  try {
+    const user = await User.findById(userId);
+    if (!userId || !user) {
+      const err = new Error("User Unauthenticated!");
+      err.statusCode = 401;
+      throw err;
+    }
+    const transactions = await User.findById(userId).populate("transactions");
+    if(transactions) {
+      const singletrans = new SingleTrans({
+        type: transactions_type,
+        amount: transactions_amount,
+        category: transactions_category
+      });
+      transactions.trans = singletrans;
+    } else {
+      const singletrans = new SingleTrans({
+        type: transactions_type,
+        amount: transactions_amount,
+        category: transactions_category
+      });
       const transaction = new Transaction({
-        transType: transType,
-        amount: amount,
-        category: category,
+        ofuser: user,
+        trans: [singletrans]
       });
       const savedTransaction = await transaction.save();
-  
-      res.status(201).json({
-        message: "Transaction saved!",
-        TransactionId: savedTransaction._id,
-      });
-    } catch (err) {
-      next(err);
     }
-  };
+    res.status(201).json({
+      message: "Transaction Added!",
+      goals_categories: goal.categories,
+      goals_limits: goal.limits
+    });
+  } catch (err) {
+    next(err);
+  }
+};
 
+// TODO: FIX UPDATE!
 
-  const getTransaction = async (req, res, next) => {
-    const TransactionId = req.TransactionId;
-  
-    try {
-      const user = await User.findById(userId);
-  
-      if (!userId || !user) {
-        const err = new Error("User Unauthenticated!");
-        err.statusCode = 401;
-        throw err;
-      }
-  
-      res.status(200).json({
-        message: "User Fetched!",
-        userId: user._id.toString(),
-        email: user.email,
-        phone: user.phone,
-        fname: user.fname,
-        lname: user.lname,
-        pages: user.pages,
-      });
-    } catch (err) {
-      next(err);
-    }
-  };
+// const update = async (req, res, next) => {
+//   const userId = req.userId;
+//   const transactions_id = req.body.tid;
+//   const transactions_type = req.body.transactions_type;
+//   const transactions_amount = req.body.transactions_amount;
+//   const transactions_category = req.body.transactions_category;
+//   try {
+//     const user = await User.findById(userId);
+//     const goal = await User.findById(userId).populate("goals");
+//     if (!userId || !user) {
+//       const err = new Error("User Unauthenticated!");
+//       err.statusCode = 401;
+//       throw err;
+//     }
+//     goal.categories = goals_categories;
+//     goal.limits = goals_limits;
+//     const savedGoal = await goal.save();
+//     res.status(201).json({
+//       message: "Goals Updated!",
+//       goals_categories: goal.categories,
+//       goals_limits: goal.limits
+//     });
+//   } catch (err) {
+//     next(err);
+//   }
+// };
